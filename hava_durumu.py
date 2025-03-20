ee792a691839a6d0deed8505cdb0130d0908f962
Semih Ada Şahin <semihadashn@gmail.com>
2025-03-20 13:37:22 +0300

    Update hava_durumu.py

--- hava_durumu.py
+++ hava_durumu.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sicakliklar = np.random.randint(-10,41,365) #Bir yıl boyunca (365 gün) günlük sıcaklık değerlerini simüle edin. Sıcaklıklar -10°C ile 40°C arasında rastgele belirlenecek.
-ortalama_sicaklik = np.mean(sicakliklar, dtype=int) #Ortalama sıcaklığı bulun.
-en_soguk_gun = np.min(sicakliklar) #En sıcak günü bulun.
-en_sicak_gun = np.max(sicakliklar) #En soğuk günü bulun.
+sicakliklar = np.random.uniform(-10,41,365) #Bir yıl boyunca (365 gün) günlük sıcaklık değerlerini simüle edin. Sıcaklıklar -10°C ile 40°C arasında rastgele belirlenecek.
+np.round(sicakliklar, 2) #Sıcaklık değerlerini 2 ondalık basamağa yuvarlayın.
+ortalama_sicaklik = round(np.mean(sicakliklar),2) #Ortalama sıcaklığı bulun. Virgülden sonrakı 2 basamağı gösterin
+en_soguk_gun = round(np.min(sicakliklar),2) #En sıcak günü bulun.
+en_sicak_gun = round(np.max(sicakliklar),2) #En soğuk günü bulun.
 print("Ortalama sıcaklık:", ortalama_sicaklik, "Derece")
 print("En soğuk gün:", en_soguk_gun, "Derece")
 print("En sıcak gün:", en_sicak_gun, "Derece")
